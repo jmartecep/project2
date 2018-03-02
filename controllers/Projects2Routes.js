008bc0a476d9aff7152feb0e8e5c6654c57cc256
Jamie <jamie.m.pena@gmail.com>
2018-03-01 21:10:42 -0500

    Second Push

--- controllers/Projects2Routes.js
+++ controllers/Projects2Routes.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models'); // Pulls out the Burger Models
+var Sequelize = require("sequelize");
 
 // Extracts the sequelize connection from the models object
 var sequelizeConnection = models.sequelize;
@@ -19,27 +20,63 @@ router.get('/', function (req, res) {
   res.redirect('/index');
 });
 
-router.get('/index', function (req, res) {
-
-      models.loc.findAll({
-
-        }).then(function(data){
-          // Pass the returned data into a Handlebars object and then render it
-          var hbsObject = { title: 'Locations', items: data }
-          res.render('index', hbsObject);
-      })
-      // models.items.findAll({
-      //   group: models.items.loc
-
-      //   }).then(function(data){
-      //     // Pass the returned data into a Handlebars object and then render it
-      //     var hbsObject = { title: 'Locations', items: data }
-      //     res.render('index', hbsObject);
-      // })
-
-
+router.get('/index', function(req, res){
+  models.loc.findAll({
+      
+  }).then(function(data){
+    var hbsObject = { title: 'Locations', items: data }
+    res.render('index', hbsObject);
+  })
 });
 
+router.get('/button1', function(req, res){
+  models.loc.findAll({
+      
+  }).then(function(data){
+    var hbsObject = { title: 'Locations', items: data }
+    res.render('index', hbsObject);
+  })
+});
+
+router.get('/button2', function(req, res){
+  models.prodtype.findAll({
+      
+  }).then(function(data){
+    var hbsObject = { title: 'Item Type', items: data }
+    res.render('index', hbsObject);
+  })
+});
+
+router.get('/button3', function(req, res){
+  models.member.findAll({
+      
+  }).then(function(data){
+    var hbsObject = { title: 'Member', items: data }
+    res.render('index', hbsObject);
+  })
+});
+
+
+
+
+// // router.get('/button1', function(req, res){
+// //   models.items.findAll({
+// //     attributes:['value']
+// //   }).then(function(data){
+// //     var allValues = {title: "Button One", items: data}
+// //     //console.log(Object.keys(allValues))
+// //     var total = 0;
+// //     for(var i = 0; i < data.length; i++){
+// //       console.log(typeof data[i].dataValues.value);
+// //       total += parseInt(data[i].dataValues.value)
+// //     }
+// //     console.log(total);
+// //     var result = {total : total}
+// //     res.render('index', result);
+// //   })
+
+
+// // })
 
 // Export routes
 module.exports = router;
